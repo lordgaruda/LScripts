364e07da1dd784e48b8982c7421d61709aba4358
lordgaruda <falgunpatel.fv@gmail.com>
2025-01-16 22:46:09 +0530

    IAM: create necessary directories while creating user

--- unix-user-iam.sh
+++ unix-user-iam.sh
@@ -3,16 +3,15 @@
 add_user() {
     local username="$1"
     local add_to_sudo="$2"
-
     if id "$username" &>/dev/null; then
         echo "User '$username' already exists."
     else
         sudo useradd -m "$username"
         echo "User '$username' has been added."
-
         echo "Setting password for '$username':"
         sudo passwd "$username"
-
+        mkdir -p /home/"$username"/{Documents,Downloads,Music,Pictures,Videos}
+        sudo chown -R "$username":"$username" /home/"$username"
         if [ "$add_to_sudo" == "yes" ]; then
             sudo usermod -aG sudo "$username"
             echo "User '$username' added to sudo group."
@@ -22,7 +21,6 @@ add_user() {
 
 remove_user() {
     local username="$1"
-
     if id "$username" &>/dev/null; then
         sudo deluser --remove-home "$username"
         echo "User '$username' has been removed."
@@ -33,7 +31,6 @@ remove_user() {
 
 echo "Do you want to add or remove a user? (add/remove)"
 read -r action
-
 if [ "$action" == "add" ]; then
     echo "Enter the username to add:"
     read -r username
